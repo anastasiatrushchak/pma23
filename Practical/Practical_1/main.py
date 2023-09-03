522f73a690e24484d87f4d5726c4cd898fa70150
Rostyslav <rostyslavpasternak@gmail.com>
2023-09-03 20:31:16 +0300

    fix practical 1

--- Practical/Practical_1/main.py
+++ Practical/Practical_1/main.py
@@ -1,14 +1,17 @@
 INPUT = 'input.txt'
 OUTPUT = 'output.txt'
 SEPARATOR = ","
-with open(INPUT, 'r') as readFile:
+
+import const
+
+with open(const.INPUT, 'r') as readFile:
     line = readFile.readline()
 
-array = line.split(SEPARATOR)
+array = line.split(const.SEPARATOR)
 array = [float(i) for i in array if i.isdigit()]
 for i in range(0, 8):
     array.append(array[i] + array[i + 1])
 
 print(array)
-with open(OUTPUT, 'w') as writeFile:
+with open(const.OUTPUT, 'w') as writeFile:
     writeFile.write(str(array))
