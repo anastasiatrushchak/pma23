3874aed7a662332a50d788250e217abbf6695950
Rostyslav <rostyslavpasternak@gmail.com>
2023-09-07 19:28:48 +0200

    add __str__, constructor from_file and vector.txt

--- Homework/Homework_1/Vector.py
+++ Homework/Homework_1/Vector.py
@@ -7,6 +7,12 @@ class Vector:
     def random(cls,size):
         vector = np.random.randint(1, 11, size = size)
         return cls(vector)
+    @classmethod
+    def from_file(cls, file_name):
+        vector = np.loadtxt(file_name, dtype=int,delimiter=" ")
+        return cls(vector)
+    def __str__(self):
+        return self.vector.__str__();
     def append(self, new_element):
         self.vector.append(new_element)
     def __add__(self, other):
@@ -16,4 +22,4 @@ class Vector:
     def __mul__(self, other):
         return self.vector * other
     def __truediv__(self, other):
-        return  self.vector - other
+        return self.vector / other
