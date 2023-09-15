5d2e412c65a197d584f12e543d0ea6498636c04a
Andrriiy <143681105+Andrriiy@users.noreply.github.com>
2023-09-15 15:38:38 +0300

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-INPUT_CODE="input.txt"
-OUTPUT_CODE="output.txt"
-with open(INPUT_CODE, "r") as read_code:
-
- array_fibonacci = read_code.read().split("\n")
-array_fibonacci = [float(i) for i in array_fibonacci if i.isdigit()]
-for i in range(2, 10):
-    array_fibonacci.append((array_fibonacci[i - 2]) + array_fibonacci[i - 1])
-
-with open(OUTPUT_CODE, "w") as output_file:
-    output_file.write(str(array_fibonacci))
+7,12
