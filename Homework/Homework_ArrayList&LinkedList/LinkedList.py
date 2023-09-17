69516a5fe15f0945623d16c58509a2356c0282df
Rostyslav <rostyslavpasternak@gmail.com>
2023-09-17 23:30:14 +0200

    create LinkedList.py and method add

---
+++ Homework/Homework_ArrayList&LinkedList/LinkedList.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+    def __str__(self):
+        return self.dataval.__str__
+class LinkedList:
+    def __init__(self):
+        self.first = Node()
+        self.last = Node()
+    def add(self,new_element):
+        if self.first.dataval == None:
+            self.first.dataval = new_element
+            self.last.dataval = new_element
+            self.first.nextval = self.last
+            return True
+        else:
+            temp = Node(new_element)
+            self.last.nextval = temp
+            self.last = temp
+            return True
+
+    def __str__(self):
+        str = ""
+        thisvalue = self.first
+        while thisvalue:
+            str += thisvalue.__str__()
+            thisvalue = thisvalue.nextval
+        return str
+    def print(self):
+        thisvalue = self.first
+        while thisvalue:
+            print(thisvalue.dataval)
+            thisvalue = thisvalue.nextval
+
+linked_list = LinkedList()
+linked_list.add(12)
+linked_list.add(11)
+linked_list.add(10)
+linked_list.add(9)
+linked_list.add(8)
+
+print(linked_list)
