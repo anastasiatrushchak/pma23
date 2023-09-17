8a772a16aa7cdd89eb41eaa16dad00dfe257d07a
Rostyslav <rostyslavpasternak@gmail.com>
2023-09-17 23:29:39 +0200

    delete unnecessary methods

--- Homework/Homework_1/standart/Matrix.py
+++ Homework/Homework_1/standart/Matrix.py
@@ -81,38 +81,6 @@ class Matrix:
             return self * other.inverse()
         elif isinstance(other, (int, float)):
             return Matrix(self.matrix/other)
-
-    # @staticmethod
-    # def det(matrix):
-    #     size = len(matrix)
-    #     if size == 2:
-    #         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    #     det = 0
-    #     for j in range(size):
-    #         submatrix = [[matrix[i][k] for k in range(size) if k != j] for i in range(1, size)]
-    #         submatrix_det = Matrix.det(submatrix)
-    #         det += ((-1) ** j) * matrix[0][j] * submatrix_det
-    #     return det
-
-    # @staticmethod
-    # def transpose(matrix):
-    #     rows, cols = len(matrix), len(matrix[0])
-    #     transpose = [[0] * rows for _ in range(cols)]
-    #     for i in range(rows):
-    #         for j in range(cols):
-    #             transpose[j][i] = matrix[i][j]
-    #     return transpose
-
-    # def minor(self, row, col):
-    #     minor = [[0 for _ in range(col - 1)] for _ in range(row - 1)]
-    #     for i in range(row):
-    #         if i != row:
-    #             minor_row = []
-    #             for j in range(self.column):
-    #                 if j != col:
-    #                     minor_row.append(self.matrix[i][j])
-    #             minor.append(minor_row)
-    #     return minor
     def inverse(self):
         if self.row != self.column:
             return None
