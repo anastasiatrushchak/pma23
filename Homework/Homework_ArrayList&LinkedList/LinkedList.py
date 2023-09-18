89d7ce2a1a72d824f43fc557cae405a0031db281
Rostyslav <rostyslavpasternak@gmail.com>
2023-09-18 10:21:16 +0200

    add previous_val in Node

--- Homework/Homework_ArrayList&LinkedList/LinkedList.py
+++ Homework/Homework_ArrayList&LinkedList/LinkedList.py
@@ -1,19 +1,22 @@
 class Node:
-    def __init__(self, dataval=None):
-        self.dataval = dataval
-        self.nextval = None
+    def __init__(self, data_val=None):
+        self.previous_val = None
+        self.data_val = data_val
+        self.next_val = None
     def __str__(self):
-        return self.dataval.__str__()
+        return self.data_val.__str__()
 class LinkedList:
     def __init__(self):
         self.first = Node()
         self.last = Node()
+        self.size = 0
     def add(self,new_element):
-        if self.first.dataval == None:
-            self.first.dataval = new_element
-            self.last.dataval = new_element
-            self.first.nextval = self.last
-            return True
+        if self.size == 0:
+            self.first.data_val = new_element
+        elif self.size == 1:
+            self.last.data_val = new_element
+            self.last.previous_val = self.first
+            self.first.next_val = self.last
         else:
             temp = Node(new_element)
             self.last.nextval = temp
@@ -25,8 +28,9 @@ class LinkedList:
         this_value = self.first
         while this_value:
             result += this_value.__str__() + " "
-            this_value = this_value.nextval
+            this_value = this_value.next_val
         return "[ " + result +" ]"
+
 linked_list = LinkedList()
 linked_list.add(12)
 linked_list.add(11)
