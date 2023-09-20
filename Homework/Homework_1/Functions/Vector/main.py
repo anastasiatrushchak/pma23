f2311bb4452f7125981a881fe56de992077f12b3
Rostyslav <rostyslavpasternak@gmail.com>
2023-09-20 14:09:30 +0200

    vector without classes

---
+++ Homework/Homework_1/Functions/Vector/main.py
@@ -0,0 +1,58 @@
+def add(first_vector, second_vector):
+    if len(first_vector) == len(second_vector):
+        size = len(first_vector)
+        result = [first_vector[i] + second_vector[i] for i in range(size)]
+        return result
+
+def sub(first_vector, second_vector):
+    if len(first_vector) == len(second_vector):
+        size = len(first_vector)
+        result = [first_vector[i] - second_vector[i] for i in range(size)]
+        return result
+def mul(first_vector, second_vector):
+    if len(first_vector) == len(second_vector):
+        size = len(first_vector)
+        result = [first_vector[i] * second_vector[i] for i in range(size)]
+        return result
+
+def div(first_vector, second_vector):
+    if len(first_vector) == len(second_vector):
+        size = len(first_vector)
+        result = [first_vector[i] / second_vector[i] for i in range(size)]
+        return result
+def str_to_file(vector,file_name="result.txt"):
+    with open(file_name, 'w') as writeFile:
+        writeFile.write(str(vector))
+
+with open("vector1.txt", 'r') as readFile:
+    line = readFile.readline()
+
+first_vector = line.split(" ")
+first_vector = [float(i) for i in first_vector if i.isdigit()]
+print("First vector: ")
+print(first_vector)
+with open("vector2.txt", 'r') as readFile:
+    line = readFile.readline()
+
+second_vector = line.split(" ")
+second_vector = [float(i) for i in second_vector if i.isdigit()]
+print("Second vector: ")
+print(second_vector)
+
+while True:
+    result = None
+    operator = int(input("1. add\n2. subtraction\n3. multiplication\n4. division\n0. Cancel\n"))
+    if operator == 1:
+        result = add(first_vector, second_vector)
+    elif operator == 2:
+        result = sub(first_vector,second_vector)
+    elif operator == 3:
+        result = mul(first_vector,second_vector)
+    elif operator == 4:
+        result = div(first_vector,second_vector)
+    else:
+        break
+    print(result)
+    str_to_file(result)
+
+
