cb715126838a52baf6dbc32635185cabb9960da8
Rostyslav <rostyslavpasternak@gmail.com>
2023-09-22 22:52:32 +0300

    fix super

--- RostyslavPasternak/Homework/Abstract_class/Color/Color.py
+++ RostyslavPasternak/Homework/Abstract_class/Color/Color.py
@@ -6,25 +6,24 @@ class Color:
 
 class Red(Color):
     def __init__(self):
-        super.__init__("red")
+        super().__init__("red")
     def __str__(self):
         return super().__str__()
 class Green(Color):
     def __init__(self):
-        super.__init__("green")
+        super().__init__("green")
     def __str__(self):
         return super().__str__()
 class Blue(Color):
     def __init__(self):
-        super.__init__("blue")
+        super().__init__("blue")
     def __str__(self):
         return super().__str__()
 class Yellow(Color):
     def __init__(self):
-        super.__init__("yellow")
+        super().__init__("yellow")
     def __str__(self):
         return super().__str__()
 
-red = Red
-
+red = Red()
 print(red)
