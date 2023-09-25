9c9f97171a6d6356577091a32dc87c58d708078f
KucheraYu <gipopotamchykjulija@gmail.com>
2023-09-26 00:09:49 +0300

    Fix + New folder

--- YuliaKuchera/Matrix/Matrix.py
+++ YuliaKuchera/Matrix/Matrix.py
@@ -75,31 +75,29 @@ def inverse_matrix(matrix):
     for i in range(n):
         identity_matrix[i][i] = 1
 
-    # Реалізація методу Гаусса-Джордана для знаходження оберненої матриці
     for col in range(n):
-        # Пошук головного елемента в стовпці
         max_row = col
         for row in range(col + 1, n):
             if abs(matrix[row][col]) > abs(matrix[max_row][col]):
                 max_row = row
 
-        # Обмін рядків для забезпечення ненульового головного елемента
         matrix[col], matrix[max_row] = matrix[max_row], matrix[col]
         identity_matrix[col], identity_matrix[max_row] = identity_matrix[max_row], identity_matrix[col]
 
-        # Ділення рядка на головний елемент, щоб зробити його рівним 1
         pivot = matrix[col][col]
-        for j in range(n):
-            matrix[col][j] /= pivot
-            identity_matrix[col][j] /= pivot
+        if pivot == 0:
+            print("ділення на 0")
+        else:
+            for j in range(n):
+                matrix[col][j] /= pivot
+                identity_matrix[col][j] /= pivot
 
-        # Відняти інші рядки від поточного рядка, щоб зробити всі інші елементи в стовпці рівними 0
-        for i in range(n):
-            if i != col:
-                factor = matrix[i][col]
-                for j in range(n):
-                    matrix[i][j] -= factor * matrix[col][j]
-                    identity_matrix[i][j] -= factor * identity_matrix[col][j]
+            for i in range(n):
+                if i != col:
+                    factor = matrix[i][col]
+                    for j in range(n):
+                        matrix[i][j] -= factor * matrix[col][j]
+                        identity_matrix[i][j] -= factor * identity_matrix[col][j]
 
     return identity_matrix
 
@@ -133,7 +131,7 @@ with open('outputM.txt', 'a') as f:
         print(r, file=f)
 
 inverse_matrix = inverse_matrix(Y)
-print(inverse_matrix)
+
 result_div = multiply_matrices(X, inverse_matrix)
 
 with open('outputM.txt', 'a') as f:
--- YuliaKuchera/MatrixClass/MatrixClass.py
+++ YuliaKuchera/MatrixClass/MatrixClass.py
@@ -63,14 +63,53 @@ class Matrix:
             result.append(row)
         return Matrix(result)
 
+    def inverse(self):
+        if len(self.matrix) != len(self.matrix[0]):
+            raise ValueError("Матриця не є квадратною, обернена матриця не існує.")
+
+        n = len(self.matrix)
+        identity = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+
+        augmented_matrix = [row[:] + identity_row for row, identity_row in zip(self.matrix, identity)]
+
+        for col in range(n):
+            pivot_row = col
+            for row in range(col + 1, n):
+                if abs(augmented_matrix[row][col]) > abs(augmented_matrix[pivot_row][col]):
+                    pivot_row = row
+            augmented_matrix[pivot_row], augmented_matrix[col] = augmented_matrix[col], augmented_matrix[pivot_row]
+
+            pivot_elem = augmented_matrix[col][col]
+            if pivot_elem == 0:
+                raise ValueError("обернена матриця не існує.")
+
+            pivot_row_scale = [elem / pivot_elem for elem in augmented_matrix[col]]
+            for row in range(n):
+                if row != col:
+                    row_scale = [elem * augmented_matrix[row][col] for elem in pivot_row_scale]
+                    augmented_matrix[row] = [elem1 - elem2 for elem1, elem2 in zip(augmented_matrix[row], row_scale)]
+
+        inv_matrix = [row[n:] for row in augmented_matrix]
+        return Matrix(inv_matrix)
+
+    def __truediv__(self, other):
+        inverse_other = other.inverse()
+        return self.__mul__(inverse_other)
+
 
 file_exists("InputM.txt")
 file_exists("InputM2.txt")
-file_exists("OutputM.txt")
+file_exists("OutputMC.txt")
 
 X = Matrix(read_matrix("InputM.txt"))
 Y = Matrix(read_matrix("InputM2.txt"))
 
-print(X + Y)
-print(X)
-print(Y)
+with open('OutputMC.txt', 'a') as f:
+    print("add:", file=f)
+    print(X + Y, file=f)
+    print("sub:", file=f)
+    print(X - Y, file=f)
+    print("mult:", file=f)
+    print(X * Y, file=f)
+    print("div:", file=f)
+    print(X / Y, file=f)
