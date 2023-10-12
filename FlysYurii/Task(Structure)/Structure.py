781b69a9c1771b9dc54aadfec2ac2f55028e2124
Yurii Flys <flysyurii@gmail.com>
2023-10-13 02:57:55 +0300

    ArrayList added

--- FlysYurii/Task(Structure)/Structure.py
+++ FlysYurii/Task(Structure)/Structure.py
@@ -8,6 +8,7 @@ class Student:
     def has_failed(self):
         return any(grade < 3 for grade in self.grades)
 
+
 def read_students_from_file(file_path):
     students = []
     with open(file_path, 'r') as file:
@@ -22,10 +23,12 @@ def read_students_from_file(file_path):
             students.append(student)
     return students
 
+
 def print_students_who_failed(students):
     for student in students:
         if student.has_failed():
             print(f"Студент {student.first_name} {student.last_name} не склав сесію.")
+
 
 students = read_students_from_file('students.txt')
 print_students_who_failed(students)
