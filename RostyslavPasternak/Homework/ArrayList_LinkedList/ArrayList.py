96710fbbff6e70769e9085810c17a311c22f732f
Rostyslav <rostyslavpasternak@gmail.com>
2023-10-12 20:47:25 +0300

    finish ArrayList

--- RostyslavPasternak/Homework/ArrayList_LinkedList/ArrayList.py
+++ RostyslavPasternak/Homework/ArrayList_LinkedList/ArrayList.py
@@ -1,24 +1,86 @@
-
 class ArrayList:
     def __init__(self, size=10):
         self.array = [None] * size
         self.size = size
+        self.capacity = 0
+    @classmethod
+    def from_list(cls, arr):
+        instance = cls()
+        instance.add(arr)
+        return instance
     def add(self,new_element):
-        if isinstance(new_element, list | tuple | set):
+        if isinstance(new_element, list):
             for element in new_element:
                 self.add(element)
         else:
             if new_element != None:
-                ...
+                self.__increase_size()
+                self.array[self.capacity] = new_element
+                self.capacity += 1
 
     def __increase_size(self):
-        temp = self.array
-        self.size = 1.5 * self.size + 1
-        self.array = [None] * int(self.size)
-        self.add(temp)
+        if self.size == self.capacity:
+            temp = self.array
+            self.size = int(1.5 * self.size + 1)
+            self.array = [None] * int(self.size)
+            self.capacity = 0
+            self.add(temp)
+    def remove_by_index(self,index):
+        self.array.pop(index)
+        self.capacity -= 1
+    def add_by_index(self, index, new_element):
+        self.array.insert(index, new_element)
+        self.capacity += 1
+        self.__increase_size()
+    def delete(self):
+        self.array = []
+        self.size = 0
+        self.capacity = 0
+
+    def __str__(self):
+        return str(self.array[0:self.capacity])
 
 
+print("<--------------------------------Create Array------------------------>")
+list1 = ArrayList.from_list([0,1,2,3,4,5,6,7,8,9])
+print(f"Size: {list1.size}")
+print(f"Capacity: {list1.capacity}")
+print(f"Array: {list1}")
 
-list = list()
+print("<--------------------------------Add New Elements------------------------>")
+list1.add(10)
+print(f"Size: {list1.size}")
+print(f"Capacity: {list1.capacity}")
+print(f"Array: {list1}")
 
-print(list.size)
+print("----")
+list1.add([11,12,13,14,15,16])
+print(f"Size: {list1.size}")
+print(f"Capacity: {list1.capacity}")
+print(f"Array: {list1}")
+
+print("<--------------------------------Remove By index------------------------>")
+list1.remove_by_index(6)
+print(f"Size: {list1.size}")
+print(f"Capacity: {list1.capacity}")
+print(f"Array: {list1}")
+
+print("<--------------------------------Add by index------------------------>")
+list1.add_by_index(6,222222)
+print(f"Size: {list1.size}")
+print(f"Capacity: {list1.capacity}")
+print(f"Array: {list1}")
+
+print("<--------------------------------Delete Array------------------------>")
+list1.delete()
+print(f"Size: {list1.size}")
+print(f"Capacity: {list1.capacity}")
+print(f"Array: {list1}")
+
+print("<--------------------------------Add new element------------------------>")
+
+list1.add(12)
+print(f"Size: {list1.size}")
+print(f"Capacity: {list1.capacity}")
+print(f"Array: {list1}")
+
