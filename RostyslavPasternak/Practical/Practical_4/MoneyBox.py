5f6e6e728e6ae78c688b4648506aca27969b05cd
Rostyslav <rostyslavpasternak@gmail.com>
2023-10-13 11:41:16 +0300

    create MoneyBox

---
+++ RostyslavPasternak/Practical/Practical_4/MoneyBox.py
@@ -0,0 +1,21 @@
+
+class MoneyBox:
+    def __init__(self, capacity=0, max=50):
+        self.__max = max
+        self.capacity = 0
+        self.add(capacity)
+    def add(self,new_coin=1):
+        if (self.capacity + new_coin)>self.__max:
+            return False
+        else:
+            self.capacity += new_coin
+            return True
+    def __str__(self):
+        return (f"Max: {self.__max}\n"
+                f"Capacity: {self.capacity}\n")
+
+box = MoneyBox(capacity=10)
+print(box)
+
+box.add(-90)
+print(box)
