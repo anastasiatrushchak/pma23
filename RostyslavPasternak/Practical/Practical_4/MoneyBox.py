4fb9e9fa52b23bb04d2c17b7d7f939c627086409
Rostyslav <rostyslavpasternak@gmail.com>
2023-10-13 15:37:31 +0300

    fix MoneyBox

--- RostyslavPasternak/Practical/Practical_4/MoneyBox.py
+++ RostyslavPasternak/Practical/Practical_4/MoneyBox.py
@@ -1,23 +1,38 @@
-
+class CountError(Exception):
+    def __init__(self):
+        super().__init__("Count Error")
 class MoneyBox:
-    def __init__(self, capacity=0, max=50):
-        self.__max = max
-        self.capacity = 0
-        self.add(capacity)
-    def add(self,new_coin=1):
-        if (self.capacity + new_coin) > self.__max or (self.capacity + new_coin) < self.capacity:
+    def __init__(self, count=0, max=50):
+        self.__capacity = max
+        self.count = 0
+        self.add(count)
+    def can_add(self, new_coin):
+        if (self.count + new_coin) > self.__capacity or new_coin < 0:
             return False
         else:
-            self.capacity += new_coin
             return True
-    def __str__(self):
-        return (f"Max: {self.__max}\n"
-                f"Capacity: {self.capacity}\n")
+    def add(self,new_coin=1):
+        temp = self.can_add(new_coin)
+        if temp:
+            self.count += new_coin
+        return temp
 
-box = MoneyBox(capacity=10)
+    def __str__(self):
+        return (f"Max: {self.__capacity}\n"
+                f"Count: {self.count}\n")
+
+box = MoneyBox()
+
+n = int(input("First number: "))
+print(box.add(n))
 print(box)
 
-print(box.add(90))
+m = int(input("First number: "))
+print(box.add(m))
+print(box)
+
+k = int(input("First number: "))
+print(box.add(k))
 print(box)
 
 
