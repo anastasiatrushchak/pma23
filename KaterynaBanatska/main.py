0a253263835e675389794494ab8b333cf1a93e7f
be3lz <143636699+be3lz@users.noreply.github.com>
2023-10-20 09:53:15 +0300

    Add files via upload

--- KaterynaBanatska/main.py
+++ KaterynaBanatska/main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+class Client:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __str__(self):
+        return f"Ім'я: {self.name}, Email: {self.email}, Телефон: {self.phone}"
 
+c1 = Client("Ivan Moroz", "ivan@example.com", "+1234567890")
+c2 = Client("Maria Havrylova", "maria@example.com", "+9876543210")
+c3 = Client("Petro Hashchuk", "petro@example.com", "+1112233445")
+c4= Client("Ivan Hashchuk", "petro@example.com", "+1112233445")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+clients = {
+    1: c1,
+    2: c2,
+    3: c3,
+    3: c4
+}
 
+for client_id, client in clients.items():
+    print(f"Client {client_id}: {client}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+try:
+    new_client = Client("Olena Lachynska", "olena@example.com", "+5555555555")
+    clients[4] = new_client
+except ValueError:
+    print("Error: Invalid data for a new client. Client not added.")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if 2 in clients:
+    del clients[2]
+
+if 1 in clients:
+    try:
+        clients[1]=c3
+    except ValueError:
+        print("Error: Invalid phone number format for client 1.")
+
+print("\n")
+for client_id, client_obj in clients.items():
+    print(f"Client {client_id}: {client_obj}")
