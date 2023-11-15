8ba07b22d3f2ba81a9f5d73634dbe4c8be5ac467
Bohdan <bohdan5621@gmail.com>
2023-11-15 21:51:30 +0200

    Add REST

--- BohdanFariyon/10.Rest/WebApp/backend/api/views.py
+++ BohdanFariyon/10.Rest/WebApp/backend/api/views.py
@@ -5,8 +5,7 @@ from todo.models import Todo
 # Create your views here.
 
 class TodoList(generics.ListAPIView):
-    # ListAPIView требует два обязательных атрибута, serializer_class и queryset.
-    # Мы указываем TodoSerializer, который мы реализовали ранее
+
     serializer_class = TodoSerializer
     def get_queryset(self):
         user=self.request.user
@@ -14,16 +13,15 @@ class TodoList(generics.ListAPIView):
 
 
 class TodoListCreate(generics.ListCreateAPIView):
-    # ListAPIView требует два обязательных атрибута, serializer_class и queryset.
-    # Мы указываем TodoSerializer, который мы реализовали ранее
+
     serializer_class = TodoSerializer
-    permission_classes = [permissions.IsAuthenticated]#доступний тільки для авторизованих, [IsAdminUser,AllowAny,IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user=self.request.user
         return Todo.objects.filter(user=user).order_by('-created')
     def perform_create(self, serializer):
-        # serializer хранит в себе модель django
+
         serializer.save(user=self.request.user)
 
 class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
@@ -31,5 +29,5 @@ class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
      permission_classes = [permissions.IsAuthenticated]
      def get_queryset(self):
          user = self.request.user
-         # пользователь может только обновлять, удалять собственные сообщения
+
          return Todo.objects.filter(user=user)
--- BohdanFariyon/10.Rest/WebApp/backend/todo/models.py
+++ BohdanFariyon/10.Rest/WebApp/backend/todo/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
     title=models.CharField(max_length=100  )
     memo=models.TextField(blank=True)
-    # установить текущее время
+
     created=models.DateTimeField(auto_now_add=True)
     completed=models.BooleanField(default=False)
-    # пользователь, разместивший это
+
     user=models.ForeignKey( User , on_delete=models.CASCADE)
     def __str__(self):
         return self.title
